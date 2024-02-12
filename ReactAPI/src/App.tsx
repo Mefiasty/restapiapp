89a96e759bf67008db1b4478b0b9602f8898b6b8
Mefiasty <97467781+Mefiasty@users.noreply.github.com>
2024-02-12 10:17:54 +0100

    Update App.tsx

--- ReactAPI/src/App.tsx
+++ ReactAPI/src/App.tsx
@@ -172,7 +172,7 @@ function App() {
           </div>
         </div>
       </div>
-      <div className="bg-black text-white">
+      <div className="bg-black text-white absolute bottom-0">
         Api used to create this page:{" "}
         <a href="https://pokeapi.co/" className="hover:text-red-700">
           PokeApi
